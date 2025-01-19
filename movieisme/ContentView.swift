af901722a53561abacfff43ba211202271074109
Wajdwael <wajdabdelhai@gmail.com>
2025-01-19 14:41:04 +0300

    Initial Commit

---
+++ movieisme/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movieisme
+//
+//  Created by Wajd Wael on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movieisme/movieismeApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieismeApp.swift
+//  movieisme
+//
+//  Created by Wajd Wael on 19/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct movieismeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
