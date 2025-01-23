b001a081309b84282438f99e4dbac3a973a001c6
Wajdwael <wajdabdelhai@gmail.com>
2025-01-23 09:59:53 +0300

    Add Sign in UI code.

--- movieisme/Views/SigninView.swift
+++ movieisme/Views/SigninView.swift
@@ -8,8 +8,115 @@
 import SwiftUI
 
 struct SigninView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @FocusState private var isFocusedEmail: Bool
+    @FocusState private var isFocusedPassword: Bool
+    @State private var isPasswordHidden: Bool = true
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            // Background images
+            Image("Signin_background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            Image("Linear_layer")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+
+            // Content
+            VStack (alignment: .leading) {
+                Spacer()
+
+                VStack (alignment: .leading, spacing: 8) {
+                    Text("Sign in")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 40, weight: .bold))
+                    Text("You'll find what you're looking for in the ocean of movies")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 18, weight: .bold))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                
+                VStack (alignment: .leading, spacing: 24) {
+                    // inputs
+                    VStack (alignment: .leading, spacing: 24) {
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Email")
+                                .foregroundStyle(Color("SecondaryTextColor"))
+                                .font(.system(size: 18, weight: .medium))
+                            
+                            TextField(
+                                "",
+                                text: $email,
+                                prompt: Text("Enter your email").foregroundColor(Color("placeholderColor"))
+                            )
+                                .accentColor(Color("MainColor"))
+                                .padding(.all, 12)
+                                .accentColor(Color.white)
+                                .foregroundColor(.white)
+                                .background(Color("LabelColor"))
+                                .cornerRadius(8)
+                            
+                                // Foocus state
+                                .focused($isFocusedEmail)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isFocusedEmail ? Color("MainColor") : Color.clear, lineWidth: 1)
+                                )
+                        }
+                        
+                        VStack (alignment: .leading, spacing: 8) {
+                            Text("Password")
+                                .foregroundStyle(Color("SecondaryTextColor"))
+                                .font(.system(size: 18, weight: .medium))
+                            
+                            SecureField(
+                                "",
+                                text: $password,
+                                prompt: Text("Enter your password").foregroundColor(Color("placeholderColor"))
+                            )
+                                .accentColor(Color("MainColor"))
+                                .padding(.all, 12)
+                                .accentColor(Color.white)
+                                .foregroundColor(.white)
+                                .background(Color("LabelColor"))
+                                .cornerRadius(8)
+                            
+                                // Foocus state
+                                .focused($isFocusedPassword)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(isFocusedPassword ? Color("MainColor") : Color.clear, lineWidth: 1)
+                                )
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    
+                    VStack {
+                        Button("Sign in") {
+                            isFocusedEmail = false
+                            isFocusedPassword = false
+                        }
+                            .font(.system(size: 18, weight: .regular))
+                            .foregroundColor(Color("LabelColor"))
+                            .frame(maxWidth: .infinity, minHeight: 44)
+                            .background(Color("SecondaryTextColor"))
+                            .cornerRadius(8)
+                    }
+                    .padding()
+                    .padding(.bottom, 50)
+
+                }
+            }
+        }
     }
 }
 
